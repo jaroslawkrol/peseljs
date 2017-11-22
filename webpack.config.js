505ce259088d74887c7014e575beb0c032fe438d
Jaroslaw Krol <Jaroslaw.Krol@vig-polska.pl>
2017-11-22 15:43:17 +0100

    Changing way of building js library by webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,19 @@ var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: "./lib/index.ts",
+    devtool: 'inline-source-map',
     output: {
         filename: "peseljs.min.js",
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        library: 'PeselJS'
     },
     resolve: {
         extensions: [".ts", ".js"]
     },
     module: {
-        loaders: [{ test: /\.ts$/, loader: "ts-loader" }]
-    }
+        loaders: [{ test: /\.ts$/, loader: "ts-loader", exclude: /node_modules/ }]
+    },
     plugins: [
         new UglifyJSPlugin()
     ]
-}
+};
